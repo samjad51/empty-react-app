505e0bea986ec1596f7db364de918824f35deee2
adam <adamchow2326@yahoo.com.au>
2021-08-12 18:19:00 +1000

    add boilierplate codes

---
+++ src/components/ResultsList/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ResultsList = () => {};
+
+ResultsList.propTypes = { className: PropTypes.string };
+
+ResultsList.defaultProps = { className: '' };
+
+export default ResultsList;
