98d3ecd18dac7c4c28b5299e77b1684b9c287938
Sami Amjad <samjad51@gmail.com>
2021-08-12 20:21:54 +0800

    remove base code

--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const SUCCESS = 'SUCCESS';
-export const ERROR = 'ERROR';
-export const LOADING = 'LOADING';
-
-export const SEARCH_API_ROUTE = '/api/search';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,5 @@
-import { SEARCH_RESULTS_SUCCESS } from 'constants/actionTypes';
-
 function rootReducer(state, action) {
     switch (action.type) {
-    case SEARCH_RESULTS_SUCCESS:
     default:
         return state;
     }
